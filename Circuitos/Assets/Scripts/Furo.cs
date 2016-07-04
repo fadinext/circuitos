01caf6676d775bcf8d1ac9f976db8a9aa1c20ca5
Ítalo Fernandes <italogsfernandes@gmail.com>
2016-07-03 23:48:41 -0300

    chão e camera adicionados, script congelado

--- Circuitos/Assets/Scripts/Furo.cs
+++ Circuitos/Assets/Scripts/Furo.cs
@@ -2,21 +2,21 @@ using System.Collections;
 using System;
 
 public class Furo {
-    public enum FuroType {
-        Source,
-        Ground,
-        Normal
-    }
-    FuroType type = Furo.Normal; //Define o defauld
-    int x;
-    int y;
-
-    public static bool hasConectionWith(this,Furo candidate) {
-        if(this.y == candidate.y) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
+//    public enum FuroType {
+//        Source,
+//        Ground,
+//        Normal
+//    }
+//    FuroType type = Furo.Normal; //Define o default
+//    int x;
+//    int y;
+//
+//    public int hasConectionWith(this, Furo candidate) {
+//        if(this.y == candidate.y) {
+//            return true;
+//        }
+//        else {
+//            return false;
+//        }
+//    }
 }
