06a57f2218751832966b083fc6b6cb3ec0c2dad2
Ítalo Fernandes <italogsfernandes@gmail.com>
2016-07-11 11:40:15 -0300

    Criado menu inicial

---
+++ Circuitos/Assets/Scripts/Controllers/MenuController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+
+	// Use this for initialization
+	public void UI_StartClicked(){
+		Application.LoadLevel("Room");
+	}
+	public void UI_AboutClicked(){
+		//TODO: Implementar cena
+		//Application.LoadLevel("SubMenu_About");
+		Debug.Log("Falta Implementar Scene_About.");
+	}
+	public void UI_ExitClicked(){
+		Debug.Log("Se tiver numa build isso vai sair, no modo de teste nao..");
+		Application.Quit();
+	}
+}
--- Circuitos/Assets/Scripts/Models/Resistor.cs
+++ Circuitos/Assets/Scripts/Models/Resistor.cs
@@ -7,6 +7,11 @@ public class Resistor {
 	public float tolerancia;
 	public Furo[] terminais;
 
+	//TODO: criar propriedades corrente e tensao
+	//Colocar no setter prase alterar 1 alterar o outro
+	public double corrente = null;
+	public double tensao = null;
+
 
 
 	public Resistor(float Resistencia, float Tolerancia, Furo[] Terminais = null){
