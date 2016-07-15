2dee4d98090e315db34b2eb7c5f69f2306273bb1
KaioSaramago <kaiosaramago@yahoo.com.br>
2016-07-14 23:38:27 -0300

    fonteCC

---
+++ Circuitos/Assets/Scripts/Models/FonteCC.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FonteCC : MonoBehaviour {
+    public enum FonteCCType
+    {
+        Corrente,
+        Tensao
+    }
+    FonteCCType type = FonteCCType.Tensao;
+    public float corrente = 0;
+    public float tensao = 0;
+    public FonteCC(float Corrente = 0,float Tensao = 0)//se o valor nao for passado considera-se 0
+    {
+        this.corrente = Corrente;
+        this.tensao = Tensao;
+    }
+
+
+}
