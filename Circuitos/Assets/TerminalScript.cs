60ea65f04bcc5c12ea257afbfa9bc26dfa7bf67e
Ítalo Fernandes <italogsfernandes@gmail.com>
2016-08-20 10:39:08 -0300

    salvando antes de apagar

--- Circuitos/Assets/TerminalScript.cs
+++ Circuitos/Assets/TerminalScript.cs
@@ -21,6 +21,7 @@ public class TerminalScript : MonoBehaviour {
 					GM.instance.AdicionarComponente (colidido);
 
 					if (col.transform.name.Contains ("Positivo")) {
+						terminalisPositivo = true;
 						if (colidido.noPositivo.Equals (-1)) {
 							no_id = GM.instance.GerarNovoNo ();
 							colidido.noPositivo = no_id;
@@ -28,8 +29,10 @@ public class TerminalScript : MonoBehaviour {
 						} else {
 							no_id = colidido.noPositivo;
 						}
+						colidido.transform.parent.BroadcastMessage ("UpdateNos", terminalisPositivo ? no_id : -no_id);
 
 					} else if (col.transform.name.Contains ("Negativo")) {
+						terminalisPositivo = false;
 						if (colidido.noNegativo.Equals (-1)) {
 							no_id = GM.instance.GerarNovoNo ();
 							colidido.noNegativo = no_id;
@@ -37,8 +40,10 @@ public class TerminalScript : MonoBehaviour {
 						} else {
 							no_id = colidido.noNegativo;
 						}
+						colidido.transform.parent.BroadcastMessage ("UpdateNos", terminalisPositivo ? no_id : -no_id);
 					}
-					transform.parent.BroadcastMessage ("UpdateNos", terminalisPositivo ? no_id : -no_id);
+
+
 					//Componente com fio:
 				} else if (col.transform.parent.tag.Equals ("Fio")) {
 					ComponenteConector colidido = col.transform.parent.GetComponent<ComponenteConector> ();
@@ -51,6 +56,7 @@ public class TerminalScript : MonoBehaviour {
 						no_id = colidido.noPrincipal;
 					}
 					transform.parent.BroadcastMessage ("UpdateNos", terminalisPositivo ? no_id : -no_id);
+					colidido.transform.parent.BroadcastMessage ("UpdateNos", terminalisPositivo ? no_id : -no_id);
 				}
 
 				//Se sou um fio
@@ -89,6 +95,7 @@ public class TerminalScript : MonoBehaviour {
 
 					if (colidido.noPrincipal.Equals (-1)) {
 						colidido.noPrincipal = no_id;
+						colidido.transform.parent.BroadcastMessage ("UpdateNos", terminalisPositivo ? no_id : -no_id);
 					} else {
 						myself.noParaUnir = colidido.noPrincipal;
 						//TODO: Como vou uni-los?
@@ -96,55 +103,6 @@ public class TerminalScript : MonoBehaviour {
 					}
 				}
 			}
-
-
-
-//
-//
-//
-//
-//
-//					string nome = col.transform.parent.name;
-//
-//			//Se colidi com um componente adiciono ele ao GM
-//			if (col.transform.parent.tag.Equals ("Componente")) {
-//				ComponenteEletrico colidido = col.transform.parent.GetComponent<ComponenteEletrico> ();
-//				GM.instance.AdicionarComponente (colidido);
-//
-//				if (col.transform.name.Contains ("Positivo")) {
-//					no_id = GM.instance.GerarNovoNo();
-//					colidido.noPositivo = no_id;
-//					GM.instance.AdicionarNo (no_id);
-//
-//				} else if(col.transform.name.Contains("Negativo") ){
-//					no_id = GM.instance.GerarNovoNo();
-//					colidido.noNegativo = no_id;
-//					GM.instance.AdicionarNo (no_id);
-//				}
-//					
-//			}
-//
-//			//se foi com a protoboard obtenho o numero do no da protoboard
-//			if (nome.Contains("Coluna")) {
-//				int.TryParse (
-//					nome.Substring (
-//						nome.IndexOf ("(") + 1,
-//						nome.IndexOf (")") - nome.IndexOf ("(") - 1),
-//					out no_id);
-//				GM.instance.AdicionarNo (no_id);
-//			}
-//
-//
-//
-//
-//			if (transform.parent.tag.Equals ("Fio")) {
-//				
-//				transform.parent.BroadcastMessage ("UnirNos");
-//			}
-//			else {
-//				transform.parent.BroadcastMessage ("UpdateNos", terminalisPositivo? no_id:-no_id);
-//			}
-//	
 		}
 	}
 }
